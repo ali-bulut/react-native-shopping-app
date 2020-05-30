d4fe92fe3b760f3b93d72b2af1012aaf71739474
Ali Bulut <alibulut@yahoo.com>
2020-05-30 18:53:13 +0300

    displaying a loading spinner and handling server errors

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,27 +1,69 @@
-import React, {useEffect} from "react";
-import { FlatList, Button, Platform } from "react-native";
-import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  Platform,
+  ActivityIndicator,
+  StyleSheet,
+} from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector, useDispatch } from "react-redux";
 
 import ProductItem from "../../components/shop/ProductItem";
-import * as cartActions from '../../store/actions/cart';
+import * as cartActions from "../../store/actions/cart";
 import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
-import Colors from '../../constants/Colors';
+import Colors from "../../constants/Colors";
 import { fetchProducts } from "../../store/actions/products";
 
 const ProductsOverviewScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   const products = useSelector((state) => state.products.availableProducts);
   const dispatch = useDispatch();
 
+  const loadProducts = useCallback(() => {
+    setError();
+    dispatch(fetchProducts()).then(() => setIsLoading(false)).catch(err => setError(err.message));
+  }, [dispatch, setIsLoading, setError])
+
   useEffect(() => {
-    dispatch(fetchProducts())
-  }, [dispatch])
+    setIsLoading(true);
+    // dispatch(fetchProducts()).then(() => setIsLoading(false)).catch(err => setError(err.message));
+    loadProducts();
+  }, [loadProducts]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate({
       routeName: "ProductDetail",
       params: { productId: id, productTitle: title },
     });
+  };
+
+  if(error){
+    return (
+      <View style={styles.centered}>
+        <Text>An Error Occured!</Text>
+        <Button title="Try Again" onPress={loadProducts} color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if(!isLoading && products.length === 0){
+    return (
+      <View style={styles.centered}>
+        <Text>No products found! Maybe start adding some?</Text>
+      </View>
+    );
   }
 
   return (
@@ -34,43 +76,63 @@ const ProductsOverviewScreen = (props) => {
           title={itemData.item.title}
           price={itemData.item.price}
           onSelect={() => {
-            selectItemHandler(itemData.item.id, itemData.item.title)
+            selectItemHandler(itemData.item.id, itemData.item.title);
           }}
         >
           <Button
-                color={Colors.primary}
-                title="View Details"
-                onPress={() => {
-                  selectItemHandler(itemData.item.id, itemData.item.title)
-                }}
-              />
-              <Button
-                color={Colors.primary}
-                title="Add To Cart"
-                onPress={() => {
-                  dispatch(cartActions.addToCart(itemData.item))
-                }}
-              />
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="Add To Cart"
+            onPress={() => {
+              dispatch(cartActions.addToCart(itemData.item));
+            }}
+          />
         </ProductItem>
       )}
     />
   );
 };
 
-ProductsOverviewScreen.navigationOptions = navData => {
-  return{
-  title: "All Products",
-  headerRight: () => <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-    <Item title="Cart" iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} onPress={() => {
-      navData.navigation.navigate('Cart')
-    }} />
-  </HeaderButtons>,
-  headerLeft: () => <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-  <Item title="Menu" iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'} onPress={() => {
-    navData.navigation.toggleDrawer();
-  }} />
-</HeaderButtons>
-  }
+ProductsOverviewScreen.navigationOptions = (navData) => {
+  return {
+    title: "All Products",
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => {
+            navData.navigation.navigate("Cart");
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ProductsOverviewScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -6,30 +6,38 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
 export const fetchProducts = () => {
-  return async (dispatch) => {
-    const response = await fetch(
-      "https://react-native-shopping-ap-61c81.firebaseio.com/products.json"
-    );
-    const resData = await response.json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          "u1",
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+  return async (dispatch) => { 
+    try{
+      const response = await fetch(
+        "https://react-native-shopping-ap-61c81.firebaseio.com/products.json"
       );
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      const resData = await response.json();
+      const loadedProducts = [];
+  
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+  
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts
+      });
+    } catch(err){
+      throw err;
     }
-
-    dispatch({
-      type: SET_PRODUCTS,
-      products: loadedProducts
-    });
+    
   };
 };
 
