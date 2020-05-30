b71cc7179952abca5a45f9f5c591a04779fd41f7
Ali Bulut <alibulut@yahoo.com>
2020-05-30 19:46:30 +0300

    fetching stored orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { FlatList, Text, Platform } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import { fetchOrders } from "../../store/actions/order";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOrders())
+  }, [dispatch])
 
   return (
     <FlatList
